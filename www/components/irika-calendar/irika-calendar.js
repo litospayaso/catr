7b7764250ddc7bcdbf0d7cdf07a5883eacd2a223
Angel Hita <angelita@MacBook-Air-de-Angel.local>
2018-06-04 12:09:14 +0200

    cleaning repository

--- www/components/irika-calendar/irika-calendar.js
+++
@@ -1,7 +0,0 @@
-/*global angular*/
-
-angular.module("starter")
-    .controller("IrikaCalendar", ["$rootScope", "$scope", function ($rootScope, $scope) {
-        "use strict";
-      // var myAppModule = angular.module('MyApp', ['ui.rCalendar']);
-    }]);
