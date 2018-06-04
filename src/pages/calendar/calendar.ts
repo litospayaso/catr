b3885e6b44533520171e613283c88121c9da08a1
Angel Hita <angelita@MacBook-Air-de-Angel.local>
2018-06-04 14:22:14 +0200

    adding calendar

--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -7,8 +7,70 @@ import { NavController } from 'ionic-angular';
 })
 export class CalendarPage {
 
+  viewTitle: string;
+
   constructor(public navCtrl: NavController) {
 
   }
 
+  calendar = {
+    mode: 'month',
+    currentDate: new Date(),
+    locale:"es-es",
+    dateFormatter: {
+      formatMonthViewDay: function (date: Date) {
+        return date.getDate().toString();
+      },
+      formatMonthViewDayHeader: function (date: Date) {
+        return 'MonMH';
+      },
+      formatMonthViewTitle: function (date: Date) {
+        return 'testMT';
+      },
+      formatWeekViewDayHeader: function (date: Date) {
+        return 'MonWH';
+      },
+      formatWeekViewTitle: function (date: Date) {
+        return 'testWT';
+      },
+      formatWeekViewHourColumn: function (date: Date) {
+        return 'testWH';
+      },
+      formatDayViewHourColumn: function (date: Date) {
+        return 'testDH';
+      },
+      formatDayViewTitle: function (date: Date) {
+        return 'testDT';
+      }
+    }
+  };
+
+  onViewTitleChanged(title) {
+    this.viewTitle = title;
+  }
+
+  onEventSelected(event) {
+    console.log('Event selected:' + event.startTime + '-' + event.endTime + ',' + event.title);
+  }
+
+  changeMode(mode) {
+    this.calendar.mode = mode;
+
+  }
+
+  onTimeSelected(ev) {
+    console.log('Selected time: ' + ev.selectedTime + ', hasEvents: ' +
+      (ev.events !== undefined && ev.events.length !== 0) + ', disabled: ' + ev.disabled);
+  }
+
+  onCurrentDateChanged(event: Date) {
+    var today = new Date();
+    today.setHours(0, 0, 0, 0);
+    event.setHours(0, 0, 0, 0);
+    // this.isToday = today.getTime() === event.getTime();
+  }
+
+  onRangeChanged(ev) {
+    console.log('range changed: startTime: ' + ev.startTime + ', endTime: ' + ev.endTime);
+  }
 }
